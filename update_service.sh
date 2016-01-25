9d055bfcdfd81ef4b775e834960eaf972bb298be
Alexei Ledenev <alexei.ledenev@hp.com>
2016-01-25 18:00:38 +0200

    update_service script is used to update single service file through GitHub API

---
+++ update_service.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ -z "$GITHUB_API_TOKEN" ]; then
+  echo "Error: GITHUB_API_TOKEN is not set!"
+  exit 1
+fi
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -f) service_file="$2"; shift;;
+    -n) service_name="$2"; shift;;
+    -b) build_num="$2"; shift;;
+    -t) branch="$2"; shift;;
+    -h)
+        echo >&2 "usage: $0 -t branch -b build -f service_file -n service_name"
+        exit 1;;
+     *) break;; # terminate while loop
+  esac
+  shift
+done
+
+f_sha=$(curl -sL "https://api.github.com/repos/gaia-adm/gaia-fleet/contents/${service_file}?ref=${branch}" | jq .sha | tr -d '"') && \
+f_64=$(openssl enc -base64 -A -in <(sed "s/gaiaadm\/${service_name}/gaiaadm\/${service_name}:${build_num}-${branch}/g" ${service_file})) && \
+curl -i -X PUT -H "Authorization: token ${GITHUB_API_TOKEN}" -d "{\"path\": \"${service_file}\", \"message\": \"CI build ${build_num}-${branch}\", \"commiter\": {\"name\": \"circleci\", \"email\": \"alexei.led@gmail.com\"}, \"content\": \"${f_64}\", \"sha\": \"${f_sha}\", \"branch\": \"${branch}\"}" https://api.github.com/repos/gaia-adm/gaia-fleet/contents/${service_file}
