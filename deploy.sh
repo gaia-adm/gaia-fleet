bc7f3ec6b0f818cc8d8e0eb7dac8dfdfa2281ed5
boriska70@gmail.com <boriska70@gmail.com>
2016-03-10 09:23:53 +0200

    added postgres service as a core service

--- deploy.sh
+++ deploy.sh
@@ -82,7 +82,7 @@ function deploy_fleet_unit() {
 }
 
 
-declare -a core_units=( skydns.service registrator.service cadvisor.service logentries.service vault.service vault-unseal.service result-upload-service.service )
+declare -a core_units=( skydns.service registrator.service postgres.service cadvisor.service logentries.service vault.service vault-unseal.service result-upload-service.service )
 declare -a all_units=(*.service)
 declare -a other_units=()
 
